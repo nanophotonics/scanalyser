3e24008ed682e9946da306c94c1926aa1db3f00d
Myron Sukhanov <myron.sukhanov@gmail.com>
2024-06-29 17:39:20 +0100

    Now inference.py can be used to load the model you want with a few lines of code.

--- inference.py
+++ inference.py
@@ -13,17 +13,27 @@ def enable_gpu():
         except RuntimeError as e:
             print(e)
 
-def load_model(ckpt_number="latest", config_to_load="version_lambda.txt"):
+def load_model(ckpt_number=None, model_config="version_lambda.txt") -> tf.keras.Model:
+    """ Load the model from the given checkpoint & the config.
+    Args:
+        ckpt_number: The checkpoint number to load the weights from.
+        model_config: The config file to get the model name from.
+    The model is loaded from the checkpoint with the provided ckpt_number.
+    If the ckpt_number is not provided, the default checkpoint is used.
+    If the ckpt_number is "latest", the latest checkpoint is used.
+    """
+
     enable_gpu()
 
     DEFAULT_CHECKPOINT = 403
+    ckpt_number = ckpt_number if ckpt_number is not None else DEFAULT_CHECKPOINT
 
     # Get the absolute path to the current file's directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Use os.path.join with current_dir to get absolute paths
     folder_path = os.path.join(current_dir, 'configs')
-    params = load_config(os.path.join(folder_path, config_to_load))
+    params = load_config(os.path.join(folder_path, model_config))
 
     # Load the model with a name from the config file. (params["c_ver"])
     Autoencoder = getattr(import_module(f"nn.models.{params['c_ver']}"), 'Autoencoder')
@@ -31,20 +41,21 @@ def load_model(ckpt_number="latest", config_to_load="version_lambda.txt"):
     enc_dir = os.path.join(current_dir, f'nn/checkpoints/cae/{params["c_ver"]}/encoder')
     dec_dir = os.path.join(current_dir, f'nn/checkpoints/cae/{params["c_ver"]}/decoder')
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=params['c_learning_rate'], clipnorm=True) # was clipnorm=False
+    print(f"I: Encoder dir: {enc_dir}")
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=params['c_learning_rate'], clipnorm=False)
     model = Autoencoder(params)
     encoder = model.layers[0]
     decoder = model.layers[1]
 
-    # If the ckpt_number is not provided, use the latest checkpoint.
     if ckpt_number == "latest":
         try:
             enc_path = tf.train.latest_checkpoint(f'{enc_dir}')
             dec_path = tf.train.latest_checkpoint(f'{dec_dir}')
         except:
             print("Failed to load the latest checkpoint. Loading the default checkpoint {DEFAULT_CHECKPOINT} instead.")
-            enc_path = rf'{enc_dir}/{DEFAULT_CHECKPOINT}'
-            dec_path = rf'{dec_dir}/{DEFAULT_CHECKPOINT}'
+            enc_path = rf'{enc_dir}/ckpt-{DEFAULT_CHECKPOINT}'
+            dec_path = rf'{dec_dir}/ckpt-{DEFAULT_CHECKPOINT}'
     else:
         try:
             enc_path = rf'{enc_dir}/ckpt-{ckpt_number}'
@@ -57,29 +68,7 @@ def load_model(ckpt_number="latest", config_to_load="version_lambda.txt"):
     encoder_ckpt = tf.train.Checkpoint(optimizer=optimizer, model=encoder)
     decoder_ckpt = tf.train.Checkpoint(optimizer=optimizer, model=decoder)
 
-    try:
-        encoder_ckpt.restore(enc_path)
-        decoder_ckpt.restore(dec_path)
-    except:
-        try:
-            encoder_ckpt.restore(enc_path).expect_partial()
-            decoder_ckpt.restore(dec_path).expect_partial()
-        except:
-            print("Failed to restore the checkpoints. Using the random initialisation instead.")
+    encoder_ckpt.restore(enc_path).expect_partial()
+    decoder_ckpt.restore(dec_path).expect_partial()
 
     return encoder, decoder
-
-def forward_pass(input_tensor, config_to_load="version_lambda.txt", ckpt_number="latest"):
-    encoder, decoder = load_model(ckpt_number, config_to_load)
-    input_tensor_expanded = tf.expand_dims(input_tensor, axis=-1)
-    output_tensor = decoder(encoder(input_tensor_expanded))
-    output_tensor = np.squeeze(output_tensor)
-    return output_tensor
-
-# if __name__ == "__main__":
-#     ckpt_number = 403 # this ckpt is used for inference
-#     config_to_load = "version_lambda.txt"
-#     encoder, decoder = load_model(ckpt_number, config_to_load)
-
-#     # Assuming input_tensor is defined and properly formatted
-#     output_tensor = forward_pass(input_tensor, encoder, decoder)
